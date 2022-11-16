fc87fc9531d369983a583606224d4414a465f0a8
luiseduardopcpereira <luiseduardopcpereira@gmail.com>
2022-11-15 22:45:33 -0300

    deploy inicial v1

---
+++ src/data/words.js
@@ -0,0 +1,9 @@
+export const wordsList = {
+  carro: ["Motor", "Porta", "Capô", "Pneu", "Antena", "volante", "buzina", "alarme", "banco", "macaco", "radiador"],
+  fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja", "goiaba", "uva", "coco", "pessego", "manga", "limão"],
+  corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos", "coração", "boca", "costas", "barriga", "figado", "joelho", "estomago"],
+  computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "usb", "internet", "fone", "microfone", "cabos"],
+  programação: ["Linguagem", "Framework", "JavaScript", "React", "Java"],
+  alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo", "lasanha", "frango", "chocolate", "doce", "brigadeiro", "pizza", "hamburguer"],
+  cep: ["Brasil", "acre", "brasilia", "riodejaneiro", "russia", "ucrania", "egito", "japao", "china", "argentina", "belem"],
+};
